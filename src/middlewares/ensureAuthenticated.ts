9a801089ccb9c6f89c5f99eddd88dc2bebc7e510
Renato Bessa <bessache@outlook.com>
2020-04-21 18:30:56 -0300

    commit aula rotas autenticadas ok

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -2,6 +2,12 @@ import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 import authConfig from '../config/auth';
 
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
 export default function ensureAuthenticated(
   request: Request,
   response: Response,
@@ -12,11 +18,15 @@ export default function ensureAuthenticated(
   if (!authHeader) {
     throw new Error('JWT token is missing');
   }
-
-  const [, token] = authHeader.split('  ');
+  const [, token] = authHeader.split(' ');
   try {
     const decoded = verify(token, authConfig.jwt.secret);
-    console.log(decoded);
+    const { sub } = decoded as TokenPayload;
+
+    request.user = {
+      id: sub,
+    };
+
     return next();
   } catch {
     throw new Error('Invalid JWT token');
