5a7883207ab2bac713fce29bb1faf5fc9fd38bf4
Renato Bessa <bessache@outlook.com>
2020-05-21 11:31:21 -0300

    comiit aula Aplicando TDD na Pratica

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,29 @@
+import { injectable, inject } from 'tsyringe';
+// import AppError from '@shared/errors/AppError';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+// import User from '../infra/typeorm/entities/User';
+import UsersRepository from '../infra/typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  email: string;
+}
+@injectable()
+class SendForgotPasswordEmail {
+  constructor(
+    @inject(UsersRepository)
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    this.mailProvider.sendMail(
+      email,
+      'Pedido de recuperacao de senha recebido',
+    );
+  }
+}
+export default SendForgotPasswordEmail;
