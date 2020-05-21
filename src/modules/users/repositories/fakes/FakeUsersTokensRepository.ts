464ac00d8a238f832fddc59e46d49683eaf3381e
Renato Bessa <bessache@outlook.com>
2020-05-21 16:18:28 -0300

    comiit aula reset de senha

--- src/modules/users/repositories/fakes/FakeUsersTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersTokensRepository.ts
@@ -16,5 +16,13 @@ class FakeIUserTokensRepository implements IUsersTokensRepository {
     this.userTokens.push(userToken);
     return userToken;
   }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(
+      findToken => findToken.token === token,
+    );
+
+    return userToken;
+  }
 }
 export default FakeIUserTokensRepository;
