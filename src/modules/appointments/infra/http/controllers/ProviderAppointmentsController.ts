14c37b896b4f3dc2c06f568a181f277e7ab3aa70
Renato Bessa <bessache@outlook.com>
2020-06-05 21:54:57 -0300

    comiit aula Express rate limit finalizaddo modulo

--- src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
-
 import { container } from 'tsyringe';
+
 import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
 
 export default class ProviderAppointmentsController {
